0793a562a6d2b9318837c7e228dd9528339b41e9
Petr Nikolas Prokop <elprokop7@gmail.com>
2020-02-14 10:10:01 +0100

    feat(_document): Update _document.ts file with new params

---
+++ src/constants/env.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = '';
+export const SITE_TITLE = '';
+export const SITE_DESCRIPTION = '';
+export const SITE_IMAGE = '';
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@
 
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { SITE_DESCRIPTION, SITE_IMAGE, SITE_NAME, SITE_TITLE } from '../constants/env';
 
 class CustomDocument extends Document {
 	static async getInitialProps(ctx) {
@@ -18,6 +19,19 @@ class CustomDocument extends Document {
 						name="viewport"
 						content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
 					/>
+					<meta property="og:type" content="website"/>
+					<meta property="og:site_name" content={SITE_NAME}/>
+					<meta property="og:title" content={SITE_TITLE}/>
+					<meta property="og:description" content={SITE_DESCRIPTION}/>
+					<meta property="og:image" content={SITE_IMAGE}/>
+					<meta name="twitter:card" content="summary_large_image"/>
+					<meta name="twitter:site" content={SITE_NAME}/>
+					<meta name="twitter:title" content={SITE_TITLE}/>
+					<meta name="twitter:description" content={SITE_DESCRIPTION}/>
+					<meta property="twitter:image" content={SITE_IMAGE}/>
+					<meta name="format-detection" content="telephone=no, address=no, email=no"/>
+
+					<link rel="shortcut icon" href="/public/favicon.ico"/>
 				</Head>
 				<body>
 					<Main/>
